727065767dc7342edaa44261bacc6598d09ff873
James Lanham <james.r.lanham2021@gmail.com>
2024-07-26 18:11:27 -0400

    Adding shelf to admin interface

--- organize_this/admin.py
+++ organize_this/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Shelving_Unit
+from .models import Shelf, Shelving_Unit
 
 # Register your models here.
 admin.site.register(Shelving_Unit)
+admin.site.register(Shelf)
