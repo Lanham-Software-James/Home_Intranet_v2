7ae071238fc9b7d5b87b3b0959e5815d9b1e1ec9
James Lanham <james.r.lanham2021@gmail.com>
2024-07-26 18:13:25 -0400

    Allow null on notes fields

--- organize_this/models.py
+++ organize_this/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Shelving_Unit(models.Model):
     name = models.CharField(max_length=100, unique=True)
     room = models.CharField(max_length=100)
-    notes = models.TextField()
+    notes = models.TextField(allow_null=True)
 
 class Shelf(models.Model):
     name = models.CharField(max_length=100, unique=True)
     shelving_unit = models.ForeignKey(Shelving_Unit, on_delete=models.DO_NOTHING)
-    notes = models.TextField()
+    notes = models.TextField(allow_null=True)
