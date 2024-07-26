b4b08ec2c272903dc559b1a662963812a6d4d614
James Lanham <james.r.lanham2021@gmail.com>
2024-07-26 17:37:42 -0400

    Describe shelf model

--- organize_this/models.py
+++ organize_this/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
-# Shelving Unit
 class Shelving_Unit(models.Model):
     name = models.CharField(max_length=100, unique=True)
     room = models.CharField(max_length=100)
+    notes = models.TextField()
+
+class Shelf(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    shelving_unit_id = models.ForeignKey(Shelving_Unit, on_delete=models.DO_NOTHING)
     notes = models.TextField()
