24042b5e7e3962387e9583b50753b28327f2fd93
James Lanham <james.r.lanham2021@gmail.com>
2024-07-27 10:58:02 -0400

    Add item to admin menu

--- organize_this/admin.py
+++ organize_this/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Shelf, Shelving_Unit, Tote
+from .models import Item, Shelf, Shelving_Unit, Tote
 
 # Register your models here.
 admin.site.register(Shelving_Unit)
 admin.site.register(Shelf)
 admin.site.register(Tote)
+admin.site.register(Item)
