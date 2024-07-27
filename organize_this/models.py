87427ba238a73156f263d012724cb5a664eaa295
James Lanham <james.r.lanham2021@gmail.com>
2024-07-27 09:15:13 -0400

    Fix nullable fields and describe Tote

--- organize_this/models.py
+++ organize_this/models.py
@@ -3,9 +3,14 @@ from django.db import models
 class Shelving_Unit(models.Model):
     name = models.CharField(max_length=100, unique=True)
     room = models.CharField(max_length=100)
-    notes = models.TextField(allow_null=True)
+    notes = models.TextField(blank=True)
 
 class Shelf(models.Model):
     name = models.CharField(max_length=100, unique=True)
     shelving_unit = models.ForeignKey(Shelving_Unit, on_delete=models.DO_NOTHING)
-    notes = models.TextField(allow_null=True)
+    notes = models.TextField(blank=True)
+
+class Tote(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    shelf = models.ForeignKey(Shelf, on_delete=models.DO_NOTHING)
+    notes = models.TextField(blank=True)
