d8886f340edac04cc0a8b35302388f44e12f94d9
James Lanham <james.r.lanham2021@gmail.com>
2024-07-27 09:47:17 -0400

    Describe Item model

--- organize_this/models.py
+++ organize_this/models.py
@@ -14,3 +14,9 @@ class Tote(models.Model):
     name = models.CharField(max_length=100, unique=True)
     shelf = models.ForeignKey(Shelf, on_delete=models.DO_NOTHING)
     notes = models.TextField(blank=True)
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    shelf = models.ForeignKey(Shelf, on_delete=models.DO_NOTHING, blank=True, null=True)
+    tote = models.ForeignKey(Tote, on_delete=models.DO_NOTHING, blank=True, null=True)
+    notes = models.TextField(blank=True)
