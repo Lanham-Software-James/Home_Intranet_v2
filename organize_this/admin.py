3e0652c5ed9887df60e11b4b9ae473fee9ed3ebf
James Lanham <james.r.lanham2021@gmail.com>
2024-07-27 10:00:25 -0400

    Adding totes to admin panel

--- organize_this/admin.py
+++ organize_this/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Shelf, Shelving_Unit
+from .models import Shelf, Shelving_Unit, Tote
 
 # Register your models here.
 admin.site.register(Shelving_Unit)
 admin.site.register(Shelf)
+admin.site.register(Tote)
